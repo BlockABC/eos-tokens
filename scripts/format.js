79dadcd8436c00f4622c3d05a1d93dbd2e91d5bf
Denis <carriere.denis@gmail.com>
2019-02-01 12:38:53 -0500

    Re-order format

---
+++ scripts/format.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
+
+for (const filepath of glob.sync(path.join(__dirname, "..", "tokens", "**", "*.json"))) {
+    const token = require(filepath)
+
+    // Format in order
+    const formatedToken = {
+        name: token.name || "",
+        symbol: token.symbol || "",
+        account: token.account || "",
+        precision: token.precision !== undefined ? token.precision : 4,
+        issuer: issuer || "",
+        logo: token.logo || "",
+        desc: token.desc || "",
+        website: token.website || "",
+        whitepaper: token.whitepaper || "",
+        links: token.links || {},
+    }
+
+    // Save Token
+    fs.writeFileSync(filepath, JSON.stringify(formatedToken, null, 2), 'utf-8')
+}
